154bcadf15d7fe26cacc8d59c2f81424213b4c77
annapey21 <anpey21@gmail.com>
2023-04-21 18:01:18 +0700

    Friday

--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -5,77 +5,143 @@ import React, { useEffect, useState } from 'react';
 const Recipe = () => {
 
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("instructions");
   let params = useParams();
 
   const fetchDetails = async () => {
-    const check = window.localStorage.getItem(params.id)
-    if (check) {
-      setDetails(JSON.parse(check))
-    } else {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/${params.id}/information?apiKey=118a264c89874b5889a1e38e60b518cd`
       );
-    
-  const detailData = await data.json();
-  setDetails(detailData);
-  window.localStorage.setItem(params.id, JSON.stringify(detailData))
+    const detailData = await data.json();
+    setDetails(detailData);
+    console.log(detailData);
   };
-}
-  
-  useEffect(() => {
-    fetchDetails();
-  }, [params.id]);
+    
+  useEffect(() => {fetchDetails()}, [params.id]);
 
-  const text = details.summary;
+  const text = details.instructions;
   const regex = /(<([^>]+)>)/gi;
-  const result = text.replace(regex, "");
+  const result = text ? text.replace(regex, "") : "";
+
 
   return (
     <DetailsWrapper>
-      <h1>{details.title}</h1>
-      <RecipeCard>
+      <div>
+        <h2>{details.title}</h2>
         <img src={details.image} alt={details.title} />
-        <p>{result}</p>
-      </RecipeCard>
+      </div>
+      <Info>
+        <ButtonWrap>
+        <Button className={activeTab === 'instructions' ? 'active' : ''} onClick={() => setActiveTab('instructions')}>Instructions</Button>
+        <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab('ingredients')}>Ingredients</Button>
+        </ButtonWrap>
+      <Content>
+        <h3>{result}</h3>
+      </Content>
+      </Info>
     </DetailsWrapper>
-    
-  );
+      
+  )
 }
 
 const DetailsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+display: flex;
+  flex-direction: row;
   justify-content: center;
-  margin: 0 auto;
-  width: 70%;
-  h1 {
-    font-size: 2rem;
-    margin: 8rem 0;
-    font-family: var(--header-font);
+  align-items: center;
+  max-width: 100%;
+  margin: 5rem 10rem;
+  .active {
+    background: transparent;
+    border: 3px solid teal;
+    color: var(--font-brown);
   }
-`;
-
-const RecipeCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 2rem;
-  background-color: white;
-  border-radius: 5px;
-  border: 1px solid red;
-  width: 100%
   img {
-    width: 100%;
+    border-radius: 10px;
+    max-width: 300px;
     height: auto;
   }
-  p {
-    margin: 2rem 0;
-    line-height: 1.2rem;
-    font-style: italic;
+  h2 {
+    margin-bottom: 2rem;
+    font-weight: 200;
     color: teal;
+    text-align: center;
   }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin: 5rem 0rem 2rem 0rem;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    img {
+      max-width: 60%;
+      height: auto;
+    } 
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  background: transparent;
+  border: 1px solid teal;
+  border-radius: 5px;
+  margin: 1rem;
+  max-width: 70%;
+  max-height: 8vh;
+  font-weight: bold;
+  color: teal;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  @media (max-width: 768px) {
+    max-width: 100%;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  max-width: 100%;
+  margin-left: 10rem;
+  h3 {
+    font-size: .8rem;
+  }
+  @media (max-width: 768px) {
+    margin: 0;
+    padding: 2rem;
+    h3 {
+      font-size: .8rem;
+    }
+  }
+  `;
+
+  const ButtonWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%; 
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+}
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  margin: 1rem;
+  line-height: 2rem;
+}
+`;
 export default Recipe;
