0bf47d185203e83bf7416cba153a1200df8203d6
annapey21 <anpey21@gmail.com>
2023-04-21 18:25:44 +0700

    fixed

--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
-import React, { useEffect, useState } from 'react';
 
 const Recipe = () => {
 
